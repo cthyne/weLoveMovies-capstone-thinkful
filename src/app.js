a6660268599ab1e6f520259042b5f6bb78035d0a
Ciara Thyne <ciara.thyne@gmail.com>
2022-02-13 21:08:13 -0800

    spelling error

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
+// DEPENDENCIES
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
-const notFound = require('./errors/notFound')
-const errorHandler = require('./errors/errorHandler')
-const moviesRouter = require('./movies/movies.router')
-const reviewsRouter = require('./reviews/reviews.router')
-const theatersRouter = require('./theaters/theaters.router')
+// ROUTES
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
-app.use(cors())
-app.use(express.json())
+// ERRORS
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 
-app.use('/movies', moviesRouter)
-app.use('/reviews', reviewsRouter)
-app.use('/theaters', theatersRouter)
+app.use(cors());
+app.use(express.json());
 
-app.use(notFound)
-app.use(errorHandler)
+// Route handlers
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
